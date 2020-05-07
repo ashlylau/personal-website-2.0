bf664426e844ced67b9d263267e5bad64011c079
Ashly Lau <ashlylau@gmail.com>
2020-05-07 17:02:13 +0800

    Updates

--- src/components/section-header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SectionHeader = ({props}) => {
-    return (
-        <h1 className='my-h1' style={{color:`${props.color}`, marginTop: `3rem`, marginBottom: `1rem`}}>{props.title}</h1>
-    )
-}
-
-export default SectionHeader
